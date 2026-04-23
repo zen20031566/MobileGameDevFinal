3433f4b0e65b7954e9a2390a99c82013c65a0a82
zen20031566 <tanzenyoung1@gmail.com>
2026-04-23 13:50:17 +0800

    jh

--- Assets/Scripts/Core/MenuManager.cs
+++ Assets/Scripts/Core/MenuManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public SceneLoader SceneLoader {  get; private set; }
+    public UnityAdsManager UnityAdsManager { get; private set; }
 
     private void Start()
     {
         SceneLoader = FindAnyObjectByType<SceneLoader>();
         if (SceneLoader == null) Debug.LogError(this + " Scene loader cannot be found");
+
+        UnityAdsManager = FindAnyObjectByType<UnityAdsManager>();
+        if (UnityAdsManager == null) Debug.LogError(this + " Unity ads manager cannot be found");
     }
 
 }
